b2e7f43b4138d110dae6d428f7cef272e455f051
tvs-vin <bradgaw007@gmail.com>
2026-04-17 11:54:13 -0400

    Made bot work, needs to be pretty and customizable still

---
+++ scripts/main.py
@@ -0,0 +1,172 @@
+# Imports
+import discord
+import json
+import os
+import sys
+import requests # pyright: ignore[reportMissingModuleSource]
+
+from discord.ext import commands
+
+# Configs
+
+with open('config/config.json', 'r') as file:
+    config = json.load(file)
+    
+with open('config/token.json', 'r') as file:
+    token = json.load(file)
+    
+with open('config/servers.json', 'r') as file:
+    servers = json.load(file)
+    
+server_fetch_time = []
+
+def reload_config():
+    with open('config/config.json', 'r') as file:
+        config = json.load(file)
+        
+    with open('config/servers.json', 'r') as file:
+        servers = json.load(file)
+
+# Intents stuff
+
+intents = discord.Intents.default()
+intents.dm_messages = True
+intents.message_content = True 
+intents.members = True 
+intents.presences = True
+
+bot = commands.Bot(command_prefix=config["PREFIX"], description="Check on your servers!",  intents=discord.Intents.default())
+client = discord.Client
+
+
+
+
+
+
+
+# / Command settup
+
+bot.tree.command(name="info", description="Gives basic info about the bot")
+async def ping_command(interaction: discord.Interaction):
+    print("Running Info")
+    await interaction.response.send_message("Made by TVS vin :wave:", ephemeral=True)
+    await bot.tree.sync()
+    
+
+
+# Sets the data for the server requested and stores it. 
+def server_data_get(server , isInList):
+    if(isInList):
+        server_path = "server-data/" + servers[server] + ".json"
+        server_data_raw = requests.request("GET",config["STATUS_API"] + servers[server],json=True)
+        server_data = server_data_raw.json()
+        print(str(server_data))
+        with open(server_path, "w") as f:
+            json.dump(server_data,f, indent=4)
+        return_obj = server_data["motd"]
+        return return_obj["raw"]
+
+
+# Restarts things (Clean reload)
+
+def restart_program():
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
+    
+
+# Sets up command base
+
+@bot.event
+async def on_ready():
+    print('TVS MC status online')
+    await startup_message(config["MESSAGE_USER_ON_STARTUP"])
+    await profile_settup()
+    
+
+@bot.event
+async def on_message(message):
+    await manual_commands(message,config["MANUAL_COMMANDS_ENABLED"])
+    
+
+# Sets up the bots profile
+
+async def profile_settup():
+    activity = discord.Game(name="ERROR - Check config")
+    
+    if(servers["FEATURED_SERVER"] != -1):
+        activity = discord.Game(name=servers[servers["FEATURED_SERVER"]])
+    
+    if(servers["FEATURED_SERVER"] == -1):
+        activity = discord.Game(name="Onboarding mode active")
+    
+    reload_config() 
+    
+    status = status_handler(config["BOT_STATUS"])
+    
+    await bot.change_presence(activity=activity,status=status)
+
+
+# Returns value based on config file. 1 - online 2 - away 3 - invis 4 - dnd
+
+def status_handler(config):
+    if(config == "online"):
+        return discord.Status.online
+    elif(config == "away"):
+        return discord.Status.idle
+    elif(config == "invisible"):
+        return discord.Status.offline
+    elif(config == "dnd"):
+        return discord.Status.dnd
+    else:
+        return discord.Status.online
+
+
+# Manual commands, can be enabled in config
+
+async def manual_commands(message,enabled):
+    if(enabled):
+        if message.author == client.user:
+            return
+        
+        if message.content.startswith(config["PREFIX_LEGACY"] + 'test'):
+            print('Recived test command (oldstle)')
+            await message.channel.send(f"Recived test command")
+            
+        if message.content.startswith(config["PREFIX_LEGACY"] + "kill"):
+            print("Recived kill command")
+            await message.channel.send(f"Ending Session")
+            quit()
+        
+        if message.content.startswith(config["PREFIX_LEGACY"] + "restart"):
+            print("Recived restart command")
+            await message.channel.send("Restarting")
+            restart_program()
+            
+        if message.content.startswith("!status"):
+            print('Recived reload status config command')
+            await message.channel.send("Reloading config")
+            await profile_settup()
+            
+        if message.content.startswith("!test-featured"):
+            print('Recived test featured server api get command')
+            await message.channel.send("Featured server test command received")
+            print(servers[servers["FEATURED_SERVER"]])
+            data = server_data_get(servers["FEATURED_SERVER"],True)
+            await message.channel.send(data)
+    
+
+# if configured to, messages user on startup
+
+async def startup_message(enabled:bool):
+    if(enabled):
+        try:
+            user = await bot.fetch_user(config["BOT_OWNER_USER_ID"])
+            await user.send(config["START_OWNER_NOTIFICATION_MESSAGE_CONTENTS"])
+            print(f"Successfully sent DM to user ID: {config["BOT_OWNER_USER_ID"]} ({user.name})")
+        except discord.NotFound:
+            print(f"Error: Could not find user with ID: {config["BOT_OWNER_USER_ID"]}. Please check the ID.")
+        except Exception as e:
+            print(f"Error sending DM to user ID {config["BOT_OWNER_USER_ID"]}. Details: {e}")
+    
+
+bot.run(token["DISCORD_BOT_TOKEN"])
---
+++ start.py
@@ -0,0 +1,74 @@
+
+# Imports
+
+import subprocess
+import sys
+import shutil
+import json
+from pathlib import Path 
+
+def filechecker() -> int:
+    config_path ="config/config.json"
+    token_path = "config/token.json"
+    servers_path = "config/servers.json"
+    
+    config_file = Path(config_path)
+    token_file = Path(token_path)
+    servers_file = Path(servers_path)
+    
+    token_settup:bool = False
+    
+    if(config_file.is_file == False):
+        shutil.copy("defaults/config.json", config_file)
+    if(servers_file.is_file == False):
+        shutil.copy("defualts/servers.json",servers_path)
+    
+    if(token_file.is_file == False):
+        print("Token not settup.")
+        token = input("Token? | ")
+        if(token != ""):
+            with open(token_path, "w") as f:
+                file_data = json.load(f)
+                file_data["DISCORD_BOT_TOKEN"] = token
+                json.dump(file_data, f, indent=4)
+                token_settup = True
+        else:
+            print("Closing")
+            quit()
+    else:
+        with open(token_path, "r") as file:
+                file_data = json.load(file)
+                if(file_data["DISCORD_BOT_TOKEN"]  == "unset"):
+                    print("Token not settup.")
+                    token = input("Token? | ")
+                    if(token != ""):
+                        with open(token_path, "w") as f:
+                            file_data = json.load(f)
+                            file_data["DISCORD_BOT_TOKEN"] = token
+                            json.dump(file_data, f, indent=4)
+                        token_settup = True
+                    else:
+                        print("Closing")
+                        quit()
+                else:
+                    token_settup = True
+                    
+    if(config_file.is_file):
+        if(token_settup):
+            if(servers_file.is_file):
+                return 1
+            else:
+                return 0
+        else:
+            return 0
+    else:
+        return 0
+
+
+settup_done:int = filechecker();
+
+if(settup_done == 1):
+    subprocess.run([sys.executable, "scripts/main.py"])
+else:
+    print("Setup incomplete. Please check the console for instructions.")
+    quit()
